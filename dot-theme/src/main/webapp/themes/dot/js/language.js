d53f0c67119d5f01f1bc9f00280fea39ae36782e
sergiofbsilva <sergiofbsilva@gmail.com>
2013-04-19 17:02:15 +0100

    Update current dot theme and layout using bootstrap

--- dot-theme/src/main/webapp/themes/dot/js/language.js
+++ dot-theme/src/main/webapp/themes/dot/js/language.js
@@ -1,7 +1,7 @@
 (function() {
 	$(".lang").click(function(e) {
 		var tag = $(e.target).text();
-		$.post("api/bennu-core/profile/locale/" + tag, null, function(data) {
+		$.post(window.contextPath + "/api/bennu-core/profile/locale/" + tag, null, function(data) {
 			location.reload();
 		});
 	});
--- dot-theme/src/main/webapp/themes/dot/js/login.js
+++ dot-theme/src/main/webapp/themes/dot/js/login.js
@@ -7,7 +7,7 @@
 	
 	$("#submit-login").click(function() {
 		var user = $("input[name=username]").val(), pass = $("input[name=password]").val();
-		$.post("api/bennu-core/profile/login", {username : user, password: pass}, function(profileAfterLogin) {
+		$.post(window.contextPath + "/api/bennu-core/profile/login", {username : user, password: pass}, function(profileAfterLogin) {
 			location.reload();
 		});
 	});
