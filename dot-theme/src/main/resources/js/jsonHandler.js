efdbdce5be056e191b8ee1aa71c4450cc4c9d4bf
sergiofbsilva <sergiofbsilva@gmail.com>
2013-05-06 11:00:57 +0100

    Minor Changes

--- dot-theme/src/main/resources/js/jsonHandler.js
+++ dot-theme/src/main/resources/js/jsonHandler.js
@@ -1,5 +1,4 @@
 function jsonHandler(hostJson) {
-	
 	if (hostJson.name) { 
 		hostJson.logged = true;
 	} else {
@@ -18,6 +17,22 @@ function jsonHandler(hostJson) {
 	$(hostJson.groups).each(function(i,e){
 		if(e["expression"] === "#managers") {
 			hostJson.manager = true;
+		}
+	});
+	
+	var path = location.href.replace(location.href.substring(0, location.href.indexOf(contextPath) + contextPath.length + 1), "");
+	var menus = new Array();
+	$(a.menu).each(function(i,menu){
+		menus.push(menu);
+		console.log(menu.path);
+		if(menu.menu){
+			$(menu.menu).each(function(i,submenu){
+				menus.push(submenu);
+				console.log(submenu.path);
+				submenu.parent = menu;
+			});
+			menus.push(menu.menu);
+			console.log(menu.menu.path);
 		}
 	});
 		
